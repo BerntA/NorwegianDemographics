1a7397cee4d36ba70c640f14e38c154d81eeaa2e
Bernt A Eide <bernta1@msn.com>
2020-02-04 13:20:40 +0000

    - Added python pip and mrjob to install script.

--- setup/setup_install.sh
+++ setup/setup_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ./
-apt-get install default-jdk && apt-get update && apt-get install -y build-essential python && \
+apt-get install default-jdk && apt-get update && apt-get install -y build-essential python && apt-get install python-pip && pip install mrjob==0.5.10 && \
 wget archive.apache.org/dist/hadoop/common/hadoop-3.1.1/hadoop-3.1.1.tar.gz && \
 tar -xzvf hadoop-3.1.1.tar.gz && \
 mv hadoop-3.1.1/* /usr/local/hadoop && \
