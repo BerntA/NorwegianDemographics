b83bd5c9568b9fe636d296781bb6b577da13b8a5
Bernt A Eide <reperio.bernt@gmail.com>
2020-02-05 14:12:58 +0000

    - Added some changes, the UiS servers use a diff java version.

--- setup/setup_env.sh
+++ setup/setup_env.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+host="127.0.0.1 localhost
+152.94.169.14 master
+192.168.12.148 slave-1
+192.168.12.51 slave-2
+192.168.12.10 slave-3
+
+# The following lines are desirable for IPv6 capable hosts
+::1 ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts"
+rm /etc/hosts && touch /etc/hosts && echo "$host" >> /etc/hosts
+
 mkdir -p /usr/local/hadoop && \
 	rm /etc/environment && \
 	touch /etc/environment && \
 	echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/hadoop/bin:/usr/local/hadoop/sbin"' >> /etc/environment && \
-	echo 'JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64/"' >> /etc/environment && \
+	echo 'JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/"' >> /etc/environment && \
 	source /etc/environment
