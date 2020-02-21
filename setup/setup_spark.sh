53c35be2f6a83c1ec7e6cc6cf0b19245dcb0a887
Bernt A Eide <bernta1@msn.com>
2020-02-21 13:22:47 +0100

    - Fixed spark setup.

--- setup/setup_spark.sh
+++ setup/setup_spark.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cd ./
 mkdir -p /usr/local/spark && \ 
 wget https://downloads.apache.org/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz && \ 
-tar -xzf spark-2.4.5-bin-hadoop2.7.tgz && mv spark-2.4.5-bin-hadoop2.7 /usr/local/spark 
+tar -xzvf spark-2.4.5-bin-hadoop2.7.tgz && \ 
+mv spark-2.4.5-bin-hadoop2.7/* /usr/local/spark && \ 
+rm spark-2.4.5-bin-hadoop2.7.tgz && \ 
+rm -r spark-2.4.5-bin-hadoop2.7
