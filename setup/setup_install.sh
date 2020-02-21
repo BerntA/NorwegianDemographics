4340c56f496988f4b76ba6e281686b704dbff087
Bernt A Eide <bernta1@msn.com>
2020-02-21 13:35:58 +0100

    - Added more fixes to spark.

--- setup/setup_install.sh
+++ setup/setup_install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-cd ./
 apt-get install default-jdk && apt-get update && apt-get install -y build-essential python && apt-get install python-pip && pip install mrjob==0.5.10 && \
 wget archive.apache.org/dist/hadoop/common/hadoop-3.1.1/hadoop-3.1.1.tar.gz && \
 tar -xzvf hadoop-3.1.1.tar.gz && \
 mv hadoop-3.1.1/* /usr/local/hadoop && \
 rm hadoop-3.1.1.tar.gz && \
-rm -r hadoop-3.1.1
-
+rm -rf hadoop-3.1.1
--- setup/setup_spark.sh
+++ setup/setup_spark.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cd ./
-mkdir -p /usr/local/spark && \ 
-wget https://downloads.apache.org/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz && \ 
-tar -xzvf spark-2.4.5-bin-hadoop2.7.tgz && \ 
-mv spark-2.4.5-bin-hadoop2.7/* /usr/local/spark && \ 
-rm spark-2.4.5-bin-hadoop2.7.tgz && \ 
-rm -r spark-2.4.5-bin-hadoop2.7
+mkdir -p /usr/local/spark && \
+wget https://downloads.apache.org/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz && \
+tar -xzvf spark-2.4.5-bin-hadoop2.7.tgz && \
+mv spark-2.4.5-bin-hadoop2.7/* /usr/local/spark && \
+rm spark-2.4.5-bin-hadoop2.7.tgz && \
+rm -rf spark-2.4.5-bin-hadoop2.7
