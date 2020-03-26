6117e9726e51da2edfda5b1ba74e1c620a064cc3
Bernt A. Eide <bernta1@msn.com>
2020-03-26 19:10:00 +0100

    - Added preprocessing mapper and reducer for hadoop. Run these to get cleaned data, afterwards use the cleaned data in Spark and or MR Job for getting interesting data for visualization.

---
+++ src/preprocess_reducer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+
+# INDEXES
+# 0 - Census Year
+# 1 - County
+# 2 - Municipality
+# 3 - Gender
+# 4 - Field of Work
+# 5 - Martial Status
+# 6 - Religion/Faith
+# 7 - Birth Year
+# 8 - Birth Place (Municipality)
+
+sys.stdin.reconfigure(encoding='utf-8') # Ensure correct encoding.
+
+for line in sys.stdin:
+    line = line.strip().split(',')
+    if len(line) != 9: # Bogus, skip!
+        continue
+
+    print(','.join(line[2:])) # Very simple reducer, we don't want the census year, not county.
