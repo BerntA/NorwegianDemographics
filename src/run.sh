110f38f9186de6114958d2f365cd7758dd5938b1
Bernt A. Eide <bernta1@msn.com>
2020-04-01 17:30:54 +0200

    - Fixed faulty municipality names, some were not using the valid 2018 names.
    - Did more preprocessing of birth places, we now have 85 percent accuracy, aka over 2 million valid names.

--- src/run.sh
+++ src/run.sh
@@ -6,21 +6,21 @@ cd ~/dat500/src
 hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.1.1.jar -file /home/ubuntu/dat500/src/preprocess_mapper.py -mapper preprocess_mapper.py -file /home/ubuntu/dat500/src/preprocess_reducer.py -reducer preprocess_reducer.py -input /data/census.csv -output /output/preprocessed && \
 
 # Move the output to our data folder.
-hadoop fs -mv -n /output/preprocessed/part* /data/census_preprocessed.csv && \
+hadoop fs -mv /output/preprocessed/part* /data/census_preprocessed.csv && \
 hadoop fs -rm -r /output/preprocessed && \
 
 # Run cleaned data through MRJob and/or Spark
 
 # Generate info about births, '<birth year>,<municipality>,<count>' ...
 python3 analyze_census_births.py --hadoop-streaming-jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.1.1.jar -r hadoop hdfs:///data/census_preprocessed.csv --output-dir hdfs:///output/analyze_births --no-output && \
-hadoop fs -mv -n /output/analyze_births/part* /data/census_births.csv && \
+hadoop fs -mv /output/analyze_births/part* /data/census_births.csv && \
 hadoop fs -rm -r /output/analyze_births && \
 
 # hadoop fs -get /data/census_births.csv ~/dat500/src/data/census_births.csv < copy over result for use in visualization
 
 # Generate info about population registered in 19XX.
 python3 analyze_census_pop.py --hadoop-streaming-jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.1.1.jar -r hadoop hdfs:///data/census_preprocessed.csv --output-dir hdfs:///output/analyze_population --no-output && \
-hadoop fs -mv -n /output/analyze_population/part* /data/census_population.csv && \
+hadoop fs -mv /output/analyze_population/part* /data/census_population.csv && \
 hadoop fs -rm -r /output/analyze_population && \
 
 # Generate info about religions based in certain municipality and how many representatives they had in 19XX.
