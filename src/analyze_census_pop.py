ec8e3ce650d3b4438c88b557e57766dddcc9f5b9
Bernt A. Eide <bernta1@msn.com>
2020-04-01 13:23:00 +0200

    - Added analysis of population, 1910.

--- src/analyze_census_pop.py
+++ src/analyze_census_pop.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
 
 from mrjob.job import MRJob
-from mrjob.step import MRStep
 
 class CSVProtocol(object):
 
     def read(self, line):
         d = line.split(',')
-        return (d[0], d[1]), int(d[2])
+        return d[0], int(d[1])
 
     def write(self, key, value):
-        return '{},{},{}'.format(key[0], key[1], value).encode()
+        return '{},{}'.format(key, value).encode()
 
 class MRAnalyzeBirths(MRJob):
 
     OUTPUT_PROTOCOL = CSVProtocol # Produce output as CSV so that we can load the results directly in our visualization notebook.
 
-    def steps(self):
-        return [
-                MRStep(mapper=self.mapper_births, reducer=self.reducer_births)
-            ]
-
-    def mapper_births(self, k, v):
+    def mapper(self, k, v):
         line = v.strip().split(',')
-        yield (line[-2], line[-1]), 1
+        yield line[0], 1
 
-    def reducer_births(self, k, v):
+    def reducer(self, k, v):
         data = list(v)
-        yield k, len(data)
+        yield k, sum(data)
 
 if __name__ == '__main__':
     MRAnalyzeBirths.run()
--- src/run.sh
+++ src/run.sh
@@ -18,6 +18,11 @@ hadoop fs -rm -r /output/analyze_births && \
 
 # hadoop fs -get /data/census_births.csv ~/dat500/src/data/census_births.csv < copy over result for use in visualization
 
+# Generate info about population registered in 19XX.
+python3 analyze_census_pop.py --hadoop-streaming-jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.1.1.jar -r hadoop hdfs:///data/census_preprocessed.csv --output-dir hdfs:///output/analyze_population --no-output && \
+hadoop fs -mv -n /output/analyze_population/part* /data/census_population.csv && \
+hadoop fs -rm -r /output/analyze_population && \
+
 # Generate info about religions based in certain municipality and how many representatives they had in 19XX.
 
 # Generate info about field of work, visualize where the most common type of work were more popular (geographically).
