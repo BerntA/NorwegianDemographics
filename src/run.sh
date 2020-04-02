2f54fe032dbba03643d1e97b2d8bcdcd16c7a90a
Bernt A. Eide <bernta1@msn.com>
2020-04-02 17:53:54 +0200

    - Updated run shell file.

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd ~/dat500/src
+# Remove old data @ hdfs
+hadoop fs -rm -r -f /data/census_* && \
 
 # Run preprocessing on raw data. Clean the data.
 hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.1.1.jar -file /home/ubuntu/dat500/src/preprocess_mapper.py -mapper preprocess_mapper.py -file /home/ubuntu/dat500/src/preprocess_reducer.py -reducer preprocess_reducer.py -input /data/census.csv -output /output/preprocessed && \
@@ -16,8 +17,6 @@ python3 analyze_census_births.py --hadoop-streaming-jar /usr/local/hadoop/share/
 hadoop fs -mv /output/analyze_births/part* /data/census_births.csv && \
 hadoop fs -rm -r /output/analyze_births && \
 
-# hadoop fs -get /data/census_births.csv ~/dat500/src/data/census_births.csv < copy over result for use in visualization
-
 # Generate info about population registered in 19XX.
 python3 analyze_census_pop.py --hadoop-streaming-jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.1.1.jar -r hadoop hdfs:///data/census_preprocessed.csv --output-dir hdfs:///output/analyze_population --no-output && \
 hadoop fs -mv /output/analyze_population/part* /data/census_population.csv && \
@@ -27,3 +26,11 @@ hadoop fs -rm -r /output/analyze_population && \
 
 # Generate info about field of work, visualize where the most common type of work were more popular (geographically).
 
+# Remove old data @ local
+rm -rf ./data/census_* && \
+
+# Copy over results from hdfs
+hadoop fs -get /data/census_births.csv ./data/census_births.csv && \
+hadoop fs -get /data/census_population.csv ./data/census_population.csv
+
+# Sync with git & open visualize.ipynb!
