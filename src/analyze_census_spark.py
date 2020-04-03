444db0ce4f8e88fe27a7b60bda307b32394d011f
Bernt A. Eide <bernta1@msn.com>
2020-04-03 17:04:38 +0200

    - Added spark analysis of our data, we now generate religion, work and gender analysis from spark SQL.

---
+++ src/analyze_census_spark.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+from pyspark.sql import SQLContext, Row
+from collections import Counter
+
+sqlContext = SQLContext(sc)
+df_census = sc.textFile('/data/census_preprocessed.csv')
+people = (df_census.map(lambda l: l.split(",")).map(lambda p: Row(municipality=p[0],gender=p[1],work=p[2],religion=p[4]))) # This is the data we're interested in.
+
+# Create our dataframe.
+schemaPeople = sqlContext.createDataFrame(people)
+schemaPeople.registerTempTable("people")
+
+# Create output with the top 20 religions in focus, the output contains data about their municipality religion and a count of how many 'members'.
+excluded_religions = set(['norwegian church', '?'])
+common_religions = Counter(sqlContext.sql("SELECT religion FROM people").collect()).most_common(20) # Select all religions and find the X most common ones!
+common_religions = set([k.religion for k,_ in common_religions if k.religion.lower() not in excluded_religions and len(k.religion) > 0])
+schemaPeople.filter(schemaPeople.religion.isin(common_religions)).groupBy('municipality', 'religion').count().orderBy("municipality").write.csv('/output/analyze_religion')
+
+# Field of Work, find the top field of work and how they are distributed around Norway!
+common_work = set(['Farmer', 'Fisher', 'Housewife', 'Maid']) # Popular field of work, discovered by running a select of all work and counting the frequencies.
+schemaPeople.filter(schemaPeople.work.isin(common_work)).groupBy('municipality', 'work').count().orderBy("municipality").write.csv('/output/analyze_work')
+
+# Gender split
+schemaPeople.groupBy('municipality', 'gender').count().orderBy("municipality").write.csv('/output/analyze_gender')
+
--- src/run.sh
+++ src/run.sh
@@ -23,11 +23,26 @@ hadoop fs -mv /output/analyze_population/part* /data/census_population.csv && \
 hadoop fs -rm -r /output/analyze_population && \
 
 # Generate info about religions based in certain municipality and how many representatives they had in 19XX.
-
 # Generate info about field of work, visualize where the most common type of work were more popular (geographically).
+# Generate info about gender bias/division.
+# Output from PySpark is merged and copied directly over to local disk.
+spark-submit --master yarn analyze_census_spark.py && \
+
+hadoop fs -getmerge -skip-empty-file /output/analyze_religion/part* ./data/census_religion.csv && \
+hadoop fs -put ./data/census_religion.csv /data/census_religion.csv && \
+hadoop fs -rm -r /output/analyze_religion && \
+
+hadoop fs -getmerge -skip-empty-file /output/analyze_work/part* ./data/census_work.csv && \
+hadoop fs -put ./data/census_work.csv /data/census_work.csv && \
+hadoop fs -rm -r /output/analyze_work && \
+
+hadoop fs -getmerge -skip-empty-file /output/analyze_gender/part* ./data/census_gender.csv && \
+hadoop fs -put ./data/census_gender.csv /data/census_gender.csv && \
+hadoop fs -rm -r /output/analyze_gender && \
 
 # Remove old data @ local
 rm -rf ./data/census_* && \
+rm -rf ./data/.census_* && \
 
 # Copy over results from hdfs
 hadoop fs -get /data/census_births.csv ./data/census_births.csv && \
